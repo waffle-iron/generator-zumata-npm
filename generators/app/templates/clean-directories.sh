e6282078911a8efacb23ce622413d2dbc76a0a53
Rong Sen Ng <wes.ngrongsen@gmail.com>
2017-04-04 21:47:24 +0800

    :art: Minor update

--- generators/app/templates/clean-directories.sh
+++ generators/app/templates/clean-directories.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-clear; printf "Running delete-directories script...\n"
+clear; printf "Running %s script...\n" "$(basename "$0" .sh)"
 
 has_unused_directories=false
 directories="dist/ node_modules/ npm-debug.log yarn-error.log .nyc_output/ coverage/ out/"
